69c322ab9900191371480ab4eabef1f019da2732
LCN <lcn225@gmail.com>
2017-12-12 23:24:26 +0800

    增加功能
    1、增加直接获得解的功能
    2、增加保存解的功能
    3、增加log

---
+++ Queen/Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class 八皇后问题
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.View_DataGridView = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nextAnswer_Button = New System.Windows.Forms.Button()
+        Me.Move_Button = New System.Windows.Forms.Button()
+        Me.Answer_ListBox = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.View_DataGridView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'View_DataGridView
+        '
+        Me.View_DataGridView.AllowUserToAddRows = False
+        Me.View_DataGridView.AllowUserToDeleteRows = False
+        Me.View_DataGridView.AllowUserToResizeColumns = False
+        Me.View_DataGridView.AllowUserToResizeRows = False
+        Me.View_DataGridView.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.View_DataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.View_DataGridView.ColumnHeadersVisible = False
+        Me.View_DataGridView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8})
+        Me.View_DataGridView.Location = New System.Drawing.Point(28, 42)
+        Me.View_DataGridView.Name = "View_DataGridView"
+        Me.View_DataGridView.ReadOnly = True
+        Me.View_DataGridView.RowHeadersVisible = False
+        Me.View_DataGridView.RowHeadersWidth = 20
+        Me.View_DataGridView.RowTemplate.Height = 23
+        Me.View_DataGridView.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.View_DataGridView.Size = New System.Drawing.Size(320, 320)
+        Me.View_DataGridView.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.Frozen = True
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        '
+        'Column2
+        '
+        Me.Column2.Frozen = True
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        '
+        'Column3
+        '
+        Me.Column3.Frozen = True
+        Me.Column3.HeaderText = "Column3"
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        '
+        'Column4
+        '
+        Me.Column4.Frozen = True
+        Me.Column4.HeaderText = "Column4"
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        '
+        'Column5
+        '
+        Me.Column5.Frozen = True
+        Me.Column5.HeaderText = "Column5"
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        '
+        'Column6
+        '
+        Me.Column6.Frozen = True
+        Me.Column6.HeaderText = "Column6"
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        '
+        'Column7
+        '
+        Me.Column7.Frozen = True
+        Me.Column7.HeaderText = "Column7"
+        Me.Column7.Name = "Column7"
+        Me.Column7.ReadOnly = True
+        '
+        'Column8
+        '
+        Me.Column8.Frozen = True
+        Me.Column8.HeaderText = "Column8"
+        Me.Column8.Name = "Column8"
+        Me.Column8.ReadOnly = True
+        '
+        'nextAnswer_Button
+        '
+        Me.nextAnswer_Button.Location = New System.Drawing.Point(91, 384)
+        Me.nextAnswer_Button.Name = "nextAnswer_Button"
+        Me.nextAnswer_Button.Size = New System.Drawing.Size(75, 23)
+        Me.nextAnswer_Button.TabIndex = 1
+        Me.nextAnswer_Button.Text = "求解"
+        Me.nextAnswer_Button.UseVisualStyleBackColor = True
+        '
+        'Move_Button
+        '
+        Me.Move_Button.Location = New System.Drawing.Point(231, 384)
+        Me.Move_Button.Name = "Move_Button"
+        Me.Move_Button.Size = New System.Drawing.Size(75, 23)
+        Me.Move_Button.TabIndex = 2
+        Me.Move_Button.Text = "移动"
+        Me.Move_Button.UseVisualStyleBackColor = True
+        '
+        'Answer_ListBox
+        '
+        Me.Answer_ListBox.FormattingEnabled = True
+        Me.Answer_ListBox.ItemHeight = 12
+        Me.Answer_ListBox.Location = New System.Drawing.Point(371, 42)
+        Me.Answer_ListBox.Name = "Answer_ListBox"
+        Me.Answer_ListBox.Size = New System.Drawing.Size(149, 244)
+        Me.Answer_ListBox.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(369, 303)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        '八皇后问题
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(532, 441)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Answer_ListBox)
+        Me.Controls.Add(Me.Move_Button)
+        Me.Controls.Add(Me.nextAnswer_Button)
+        Me.Controls.Add(Me.View_DataGridView)
+        Me.Name = "八皇后问题"
+        Me.Text = "Form1"
+        CType(Me.View_DataGridView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents View_DataGridView As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents nextAnswer_Button As Button
+    Friend WithEvents Move_Button As Button
+    Friend WithEvents Answer_ListBox As ListBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Queen/Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports System.IO
+Public Class 八皇后问题
+
+    Dim loc(0 To 7) As Integer
+    Dim locLine As String
+    Dim answerNum As Integer = 1
+    Dim queenNum As Integer = 1
+    Dim testMode As Boolean = False
+
+    Private Sub formatDGV()
+
+        For index = 1 To 8
+            View_DataGridView.Rows.Add()
+            View_DataGridView.Rows(index - 1).Height = 40
+        Next
+
+        For index = 1 To 8
+            View_DataGridView.Columns(index - 1).Width = 40
+        Next
+
+    End Sub
+
+    Private Sub displayDGV()
+
+        For index = 1 To 8
+            For j = 1 To 8
+                View_DataGridView.Rows(index - 1).Cells(j - 1).Value = ""
+            Next
+        Next
+
+        For index = 1 To 8
+            If loc(index - 1) > 0 Then
+                View_DataGridView.Rows(index - 1).Cells(loc(index - 1) - 1).Value = "○"
+            End If
+        Next
+
+    End Sub
+
+    Private Sub textshow()
+
+        Label1.Text = "目前：" & queenNum & vbCrLf
+
+        For index = 1 To 8
+            Label1.Text = Label1.Text & index & ": " & index & "," & loc(index - 1) & vbCrLf
+        Next
+    End Sub
+
+    Private Sub movequeen(i As Integer)
+        ''i即为queenNum，从1~8
+
+        Dim k As Integer
+        ''定义比较棋子的编号
+
+        If i = 1 Then
+            loc(i - 1) += 1
+            ''放置第一行的棋子
+            queenNum += 1
+            Exit Sub
+            ''如果是第一行的棋子，放好后直接结束
+        End If
+
+        For j = (loc(i - 1) + 1) To 8
+            ''尝试第j列，从loc(i)+1开始。
+            Dim flag As Integer = 0
+            ''设置flag为0，开始比较之前的棋子
+            For k = 1 To i - 1
+                    ''从第一个棋子开始对比是否冲突，直到前一个棋子
+                    If j = loc(k - 1) Or Math.Abs(i - k) = Math.Abs(j - loc(k - 1)) Then
+                        flag += 1
+                        ''如果有冲突，flag+1，不比较剩下的棋子
+                        Exit For
+                        ''如果没有冲突，则比较下一个结果
+                    End If
+                Next
+                ''所有棋子比较完毕
+                If flag = 0 Then
+                    loc(i - 1) = j
+                    queenNum += 1
+                    Exit Sub
+                    ''如果没冲突，记录列数，结束
+                End If
+            ''如果有冲突，继续
+        Next
+
+        ''到达这里表示全部列比较完毕仍然有冲突
+
+        For n = i - 1 To 7
+            loc(n) = 0
+        Next
+        ''消除之后的所有结果
+
+        i = i - 1
+        queenNum = queenNum - 1
+        ''从上一个棋子开始
+        movequeen(i)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        formatDGV()
+        textshow()
+    End Sub
+
+    Private Sub Move_Button_Click(sender As Object, e As EventArgs) Handles Move_Button.Click
+        movequeen(queenNum)
+
+        textshow()
+        displayDGV()
+    End Sub
+
+    Private Sub locToString()
+        locLine = loc(0) & " " & loc(1) & " " & loc(2) & " " & loc(3) & " " & loc(4) & " " & loc(5) & " " & loc(6) & " " & loc(7)
+    End Sub
+    ''将当前放置方法生产字符串
+
+    Private Sub answerToLB()
+        locToString()
+        Answer_ListBox.Items.Add(answerNum & ": " & locLine)
+        answerNum += 1
+    End Sub
+    ''将答案写入LB
+
+    Private Sub nextAnswer_Button_Click(sender As Object, e As EventArgs) Handles nextAnswer_Button.Click
+
+        FileOpen(1, "Log.txt", OpenMode.Output)
+
+        If queenNum = 9 Then
+            queenNum = 8
+        End If
+        ''如果放好8个棋子了，则继续动第八个棋子（即继续找下一个解）
+
+        While queenNum < 9
+            movequeen(queenNum)
+
+            If testMode Then
+                locToString()
+                PrintLine(1, locLine & vbCrLf)
+            End If
+            ''调试模式，写入LOG
+
+        End While
+        ''当放好8个棋子之前，重复Move
+
+        answerToLB()
+        ''将答案写入LB
+
+        textshow()
+        displayDGV()
+
+        FileClose(1)
+
+    End Sub
+End Class
